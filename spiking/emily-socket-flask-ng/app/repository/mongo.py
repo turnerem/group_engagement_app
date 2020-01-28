870bb1d44377e635caaec56b6f56953849f9ca95
mbaxret2 <emily.turner@manchester.ac.uk>
2020-01-28 12:41:31 +0000

    update concrete class

--- spiking/emily-socket-flask-ng/app/repository/mongo.py
+++ spiking/emily-socket-flask-ng/app/repository/mongo.py
@@ -10,6 +10,13 @@ class MongoRepository(object):
     self.db = MongoClient(mongo_url).presentation
 
   # This is the bridge between python and mongo db request syntax
+  # selector is an object with keys. If we want all presentations for a given user:
+  # it would look something like {'user_id': self.user_id} in app/pres/service.py
+  # Or if selecting one pres for that user:
+  # than {'user_id':self.user_id, 'presentation_id': presentation_id} (no self on presentation...?)
+  def find_all(self, selector):
+    return self.db.presentation.find(selector)
+
   def find(self, selector):
     return self.db.presentation.find_one(selector)
 
