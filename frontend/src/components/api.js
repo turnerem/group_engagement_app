f58f3c8501e6803558f1200ccb327c907bf99b43
douglashellowell <doog99@gmail.com>
2020-01-31 12:23:54 +0000

    add post request for new user account creation

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
-const baseURL = "http://192.168.100.131:5000/api/JessJelly";
+const baseURL = "http://192.168.100.131:5000/api";
 
 export const getSessions = () => {
-  return axios.get(`${baseURL}`).then(({ data }) => {
+  return axios.get(`${baseURL}JessJelly`).then(({ data }) => {
     return data.sessions;
+  });
+};
+
+export const postNewUser = username => {
+  console.log("attemping post w");
+  return axios.post(`${baseURL}`, { user_name: username }).then(({ data }) => {
+    console.log(data);
+    return data;
   });
 };
