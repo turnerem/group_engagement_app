74aa659c1ec5a37ee6a593142dc5db9d0e3f1e3c
douglashellowell <doog99@gmail.com>
2020-02-03 13:08:58 +0000

    set up Audience view for socket testing

--- frontend/src/components/Audience/Audience.js
+++ frontend/src/components/Audience/Audience.js
@@ -1,12 +1,28 @@
-import React from "react";
 import "./Audience.css";
+import React, { Component } from "react";
 
-const Audience = ({ room_code }) => {
-  return (
-    <div className="audience-view-container">
-      <p>Audienceview: {room_code}</p>
-    </div>
-  );
-};
+class Audience extends Component {
+  state = {};
+
+  componentDidMount() {
+    console.log(`...mounting: `);
+    console.log(
+      `%c room_code: ${this.props.room_code}`,
+      "background:#000; color:#bada55;"
+    );
+  }
+
+  render() {
+    const { room_code } = this.props;
+    console.log(`...rendering: `);
+    console.log(`%c room_code: ${room_code}`, "background:#000; color:red;");
+    return (
+      <div className="audience-view-container">
+        <p>room_code: {room_code}</p>
+        <p>You are in a room with ___ others</p>
+      </div>
+    );
+  }
+}
 
 export default Audience;
